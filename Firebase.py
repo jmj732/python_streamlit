6e50386ddaf8dfcd4a4a11516711f3f4259dabd4
jaeminjo732 <jaeminjo732@gmail.com>
2024-07-16 12:09:08 +0900

    2

--- Firebase.py
+++ Firebase.py
@@ -1,8 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-
-cred = credentials.Certificate('secrets.json')
+import json
+import streamlit as st
+key_dict = json.loads(st.secrets["textkey"])
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate(key_dict)
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://python-59598-default-rtdb.firebaseio.com'
